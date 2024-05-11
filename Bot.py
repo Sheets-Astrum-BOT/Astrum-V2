4ac58bd8fb3937d4920ef2a753547ae805356e3c
Soham Maity <sohammaity006@gmail.com>
2024-05-11 19:34:46 +0530

    Added Tic Tac Toe

--- Bot.py
+++ Bot.py
@@ -10,6 +10,7 @@ import datetime
 import requests
 
 import discord
+from typing import List
 from discord.ext import commands, tasks
 
 bot = discord.Bot()
@@ -131,18 +132,120 @@ class MeaningView(discord.ui.View):
             embed.add_field(name="Example", value=meaning['example'], inline=False)
         embed.set_footer(text=f"Page {self.current_meaning_index + 1}/{len(self.meanings)}")
         await self.ctx.respond(embed=embed)
+        
+class TicTacToeButton(discord.ui.Button["TicTacToe"]):
+    def __init__(self, x: int, y: int):
+
+        super().__init__(style=discord.ButtonStyle.secondary, label="\u200b", row=y)
+        self.x = x
+        self.y = y
+        
+    async def callback(self, interaction: discord.Interaction):
+        assert self.view is not None
+        view: TicTacToe = self.view
+        state = view.board[self.y][self.x]
+        if state in (view.X, view.O):
+            return
+
+        if view.current_player == view.X:
+            self.style = discord.ButtonStyle.danger
+            self.label = "X"
+            view.board[self.y][self.x] = view.X
+            view.current_player = view.O
+            content = "It Is Now O's Turn"
+        else:
+            self.style = discord.ButtonStyle.success
+            self.label = "O"
+            view.board[self.y][self.x] = view.O
+            view.current_player = view.X
+            content = "It Is Now X's Turn"
+
+        self.disabled = True
+        winner = view.check_board_winner()
+        if winner is not None:
+            if winner == view.X:
+                content = "X Won"
+            elif winner == view.O:
+                content = "O Won"
+            else:
+                content = "It's A Tie"
+
+            for child in view.children:
+                child.disabled = True
+
+            view.stop()
+
+        await interaction.response.edit_message(content=content, view=view)
 
 
+class TicTacToe(discord.ui.View):
 
+    children: List[TicTacToeButton]
+    X = -1
+    O = 1
+    Tie = 2
+
+    def __init__(self):
+        super().__init__()
+        self.current_player = self.X
+        self.board = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+        ]
+
+        for x in range(3):
+            for y in range(3):
+                self.add_item(TicTacToeButton(x, y))
+
+    def check_board_winner(self):
+
+        for across in self.board:
+            value = sum(across)
+            if value == 3:
+                return self.O
+            elif value == -3:
+                return self.X
+
+        for line in range(3):
+            value = self.board[0][line] + self.board[1][line] + self.board[2][line]
+            if value == 3:
+                return self.O
+            elif value == -3:
+                return self.X
+
+        diag = self.board[0][2] + self.board[1][1] + self.board[2][0]
+        if diag == 3:
+            return self.O
+        elif diag == -3:
+            return self.X
+
+        diag = self.board[0][0] + self.board[1][1] + self.board[2][2]
+        if diag == -3:
+            return self.X
+        elif diag == 3:
+            return self.O
+
+        if all(i != 0 for row in self.board for i in row):
+            return self.Tie
+
+        return None
+
+    
+@bot.slash_command(name="tictactoe", description="Play Tic Tac Toe With An User")
+async def tictactoe(ctx: commands.Context):
+    await ctx.respond("Tic Tac Toe: X Goes First :D", view=TicTacToe())
+    
 bot.load_extension('Cogs.Fun')
 bot.load_extension('Cogs.AFK')
 bot.load_extension('Cogs.Help')
 bot.load_extension('Cogs.Anime')
 bot.load_extension('Cogs.Utils')
+# bot.load_extension('Cogs.ModelAI')
 bot.load_extension('Cogs.Reminder')
 bot.load_extension('Cogs.Moderation')
 bot.load_extension('Cogs.Information')
 bot.load_extension('Cogs.Entertainment')
 
 
-bot.run('')
+bot.run('MTExMTU1NTY1MjYxMjAxODI0Ng.GZkFKu.mX-lXXeL6VwOBDt3W6JaygJQlHJF9iYCKQ2wAE')
